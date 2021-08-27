a4200420b67add93abc7fd8ba1b3eeab9d5deccf
blyzniuk <blyzniuk.oleksandr@gmail.com>
2021-08-27 19:58:25 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", async (req, res) => {
+  res.json({ status: "OK" });
+});
+
+app.get("/hello", async (req, res) => {
+  res.json({ message: "world" });
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
